aa73e29e8fc1871cf9d56b48490eefd1cc42c6ac
viveksheelsingh <78414297+viveksheelsingh@users.noreply.github.com>
2022-02-17 21:59:47 +0530

    Adding mock srcdataplane

---
+++ SamplePlugin/MockSourceDataplane.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace SamplePlugin
+{
+    /// <summary>
+    /// Barebone mock source dataplane.
+    /// </summary>
+    internal static class MockSourceDataplane
+    {
+        private static string backupContent = "Just a small random backup content";
+        public static int maxReadSize = 1024;
+
+        public static Stream DoBackup()
+        {
+            return new MemoryStream(Encoding.ASCII.GetBytes(backupContent));
+        }
+
+        public static void DoRestore(Stream stream)
+        {
+            byte[] buffer = new byte[maxReadSize];
+            stream.ReadAsync(buffer, 0, maxReadSize);
+            string restoredContent = Encoding.ASCII.GetString(buffer);
+        }
+        
+    }
+}
