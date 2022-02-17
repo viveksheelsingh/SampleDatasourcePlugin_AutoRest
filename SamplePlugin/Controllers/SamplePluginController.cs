bee569164ef00cef329e806fee0ecd06d49cb275
viveksheelsingh <78414297+viveksheelsingh@users.noreply.github.com>
2022-02-17 21:16:27 +0530

    Adding operations class

--- SamplePlugin/Controllers/SamplePluginController.cs
+++ SamplePlugin/Controllers/SamplePluginController.cs
@@ -7,34 +7,7 @@ using System.Net.Http;
 
 namespace SamplePlugin.Controllers
 {
-    /// <summary>
-    /// Plugin specific LoopBackMetadata class. See https://msazure.visualstudio.com/One/_wiki/wikis/DppDocumentation/224106/Loopback-Context
-    /// </summary>
-    internal class LoopBackMetadata
-    {
-        public string Foo { get; set; }
-        public string Bar { get; set; }
-        public string ErrorCode { get; set; }
-    }
-
-    /// <summary>
-    /// Plugin specific implementation for maintaning the operations collection
-    /// </summary>
-    internal class OperationDetails
-    {
-        public string OperationPayload { get; set; } // plugin specific data
-        public string OperationKind { get; set; }
-        public string Status { get; set; }
-        public DateTime? StartTime { get; set; }
-        public DateTime? CreatedTime { get; set; }
-        public DateTime? EndTime { get; set; }
-        public DateTime? PurgeTime { get; set; }
-        public Error? OpError { get; set; }
-        public HttpStatusCode? StatusCode { get; set; }
-    }
-
-
-
+    
     [ApiController]
     public class SamplePluginController : ControllerBase
     {
@@ -91,7 +64,7 @@ namespace SamplePlugin.Controllers
         }
         #endregion
 
-        #region Plugin Verbs
+        #region Plugin Verb Controllers
 
         [HttpPost]
         [Route("/plugin:ValidateForProtection")]
---
+++ SamplePlugin/Operations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AzureBackup.DatasourcePlugin.Models;
+using System.Net;
+
+namespace SamplePlugin
+{
+    /// <summary>
+    /// Plugin specific LoopBackMetadata class. See https://msazure.visualstudio.com/One/_wiki/wikis/DppDocumentation/224106/Loopback-Context
+    /// </summary>
+    internal class LoopBackMetadata
+    {
+        public string Foo { get; set; }
+        public string Bar { get; set; }
+        public string ErrorCode { get; set; }
+    }
+
+    /// <summary>
+    /// Plugin specific implementation for maintaning the operations collection
+    /// </summary>
+    internal class OperationDetails
+    {
+        public string OperationPayload { get; set; } // plugin specific data
+        public string OperationKind { get; set; }
+        public string Status { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public DateTime? PurgeTime { get; set; }
+        public Error? OpError { get; set; }
+        public HttpStatusCode? StatusCode { get; set; }
+    }
+
+}
