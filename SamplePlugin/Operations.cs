7186ea80ce13cc54dc937afbffa352f7e0bff9c8
viveksheelsingh <78414297+viveksheelsingh@users.noreply.github.com>
2022-02-17 21:35:55 +0530

    Refctor operations

--- SamplePlugin/Operations.cs
+++ SamplePlugin/Operations.cs
@@ -27,6 +27,56 @@ namespace SamplePlugin
         public DateTime? PurgeTime { get; set; }
         public Error? OpError { get; set; }
         public HttpStatusCode? StatusCode { get; set; }
+
+        public static OperationDetails CreateOperationDetails(DateTime createdTime, Response.KindEnum kind)
+        {
+            return new OperationDetails()
+            {
+                CreatedTime = createdTime,
+                EndTime = null,
+                PurgeTime = null,
+                OperationPayload = "custom plugin payload",
+                StartTime = createdTime,
+                Status = Microsoft.AzureBackup.DatasourcePlugin.Models.Response.StatusEnum.RunningEnum.ToString(),
+                OperationKind = kind.ToString(),
+                OpError = null,
+                StatusCode = null,
+            };
+        }
+    }
+
+    internal static class OperationsMap
+    {
+        /// <summary>
+        /// Plugin specific implementation for maintaning the operations collection.
+        /// This sample uses an in-memory dictionary. 
+        /// For more details, please refer to: https://msazure.visualstudio.com/One/_wiki/wikis/DppDocumentation/220542/Idempotency-and-crash-scenarios
+        /// </summary>
+        private static Dictionary<string, OperationDetails> opMap = new Dictionary<string, OperationDetails>();
+
+       
+        /// <summary>
+        /// Adds a new operation to the operations map.
+        /// </summary>
+        /// <param name="operationid"></param>
+        public  static void AddToOperationsMap(string operationid, OperationDetails opDetails)
+        {
+            opMap.Add(operationid, opDetails);
+        }
+
+        /// <summary>
+        /// Get status of a given operation
+        /// </summary>
+        /// <param name="operationid"></param>
+        /// <returns></returns>
+        public  static OperationDetails GetOperationDetails(string operationid)
+        {
+            if (opMap.TryGetValue(operationid, out var opDetails))
+            {
+                return opDetails;
+            }
+            else return null;
+        }
     }
 
 }
